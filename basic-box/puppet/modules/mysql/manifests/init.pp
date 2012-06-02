f2b626513f6e5d903fffb97ebdbe66d874307192
Jaromír Müller <jaromir.muller@gmail.com>
2012-06-02 12:39:51 +0300

    added mysql puppet modules (from puppetlabs-mysql)
    Change-Id: I416a4999919226960c92a8a269b29eea7f6b22a1

---
+++ basic-box/puppet/modules/mysql/manifests/init.pp
@@ -0,0 +1,30 @@
+# Class: mysql
+#
+#   This class installs mysql client software.
+#
+# Parameters:
+#   [*client_package_name*]  - The name of the mysql client package.
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class mysql (
+  $package_client_name   = $mysql::params::client_package_name,
+  $package_server_name   = $mysql::params::server_package_name,
+  $package_ensure = 'latest'
+) inherits mysql::params {
+
+  package { 'mysql_client':
+    name    => $package_client_name,
+    ensure  => $package_ensure,
+  }
+
+  package { 'mysql_server':
+    name    => $package_server_name,
+    ensure  => $package_ensure,
+  }
+
+}
